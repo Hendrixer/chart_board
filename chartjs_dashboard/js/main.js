ee8da04bdafe6070ef2a42a1c3cff6d2277e29c4
Hendrixer <william@readerup.com>
2013-08-16 00:31:17 -0700

    get rid of example data

--- chartjs_dashboard/js/main.js
+++ chartjs_dashboard/js/main.js
@@ -52,46 +52,6 @@ function redraw(animation){
 	var ctx = canvas.getContext("2d");
 	new Chart(ctx).Doughnut(data, options);
 
-/*	var data = {
-		labels : ["Mon","Tue","Wed","Thu","Fri","Sat","Sun"],
-		datasets : [
-			{
-				fillColor : "rgba(99,123,133,0.4)",
-				strokeColor : "rgba(220,220,220,1)",
-				pointColor : "rgba(220,220,220,1)",
-				pointStrokeColor : "#fff",
-				data : [65,54,30,81,56,55,40]
-			},
-			{
-				fillColor : "rgba(219,186,52,0.4)",
-				strokeColor : "rgba(220,220,220,1)",
-				pointColor : "rgba(220,220,220,1)",
-				pointStrokeColor : "#fff",
-				data : [20,60,42,58,31,21,50]
-			},
-		]
-	}
-	var canvas = document.getElementById("shipments");
-	var ctx = canvas.getContext("2d");
-	new Chart(ctx).Line(data, options); */
-
-
-
-/*	var data = {
-		labels : ["Helpful","Friendly","Kind","Rude","Slow","Frustrating"],
-		datasets : [
-			{
-				fillColor : "rgba(220,220,220,0.5)",
-				strokeColor : "#637b85",
-				pointColor : "#dbba34",
-				pointStrokeColor : "#637b85",
-				data : [65,59,90,81,30,56]
-			}
-		]
-	}
-	var canvas = document.getElementById("departments");
-	var ctx = canvas.getContext("2d");
-	new Chart(ctx).Radar(data, options);*/
 }
 size(true);
 
