df44a1172ec2179b4dde3d0884209508eba1e29f
Hendrixer <william@readerup.com>
2013-08-18 15:05:34 -0700

    add line graph in main.js

--- js/main.js
+++ js/main.js
@@ -52,6 +52,29 @@ function redraw(animation){
 	var ctx = canvas.getContext("2d");
 	new Chart(ctx).Doughnut(data, options);
 
+	var data = {
+		labels : ["Mon", "Tues", "Weds", "Thurs", "Fri", "Sat", "Sun"],
+		datasets : [
+			{
+				fillColor : "rgba(99,123,133,0.4)",
+	            strokeColor : "rgba(220,220,220,1)",
+	            pointColor : "rgba(220,220,220,1)",
+	            pointStrokeColor : "#fff",
+	            data : [65,54,30,81,56,55,40]
+			},
+			{
+				fillColor : "rgba(219,186,52,0.4)",
+	            strokeColor : "rgba(220,220,220,1)",
+	            pointColor : "rgba(220,220,220,1)",
+	            pointStrokeColor : "#fff",
+	            data : [20,60,42,58,31,21,50]
+			}
+		]
+	}
+	var canvas = document.getElementById("shipments");
+	var ctx = canvas.getContext("2d");
+	new Chart(ctx).Line(data, options); 
+
 }
 size(true);
 
